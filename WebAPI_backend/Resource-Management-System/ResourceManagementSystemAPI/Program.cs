c8adfe8b8ef6e0cebc31898efcdd739652920842
Ellowa <ellowa123321@gmail.com>
2023-04-14 17:30:57 +0300

    Add MySQL connection + register DbContext

--- WebAPI_backend/Resource-Management-System/ResourceManagementSystemAPI/Program.cs
+++ WebAPI_backend/Resource-Management-System/ResourceManagementSystemAPI/Program.cs
@@ -1,3 +1,7 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkMySQL().AddDbContext<ApplicationDbContext>(options => {
+    options.UseMySQL(builder.Configuration.GetConnectionString("MySQLConnection"));
+});
 
 var app = builder.Build();
 
