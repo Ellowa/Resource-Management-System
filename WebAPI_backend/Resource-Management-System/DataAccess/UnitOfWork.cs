2ed4fae5e3c9c006364fb93e778e9db8b6c9048b
Ellowa <ellowa123321@gmail.com>
2023-04-14 18:19:34 +0300

    Add empty UnitOfWork

---
+++ WebAPI_backend/Resource-Management-System/DataAccess/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
