c47356b732f5a9eee95d59850c74a6e38704dc85
Ellowa <ellowa123321@gmail.com>
2023-04-21 18:20:03 +0300

    Added Models

---
+++ WebAPI_backend/Resource-Management-System/BysinessServices/Models/ResourceModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BysinessServices.Models
+{
+    public class ResourceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public int ResourceTypeId { get; set; }
+
+        public string ResourceTypeName { get; set; }
+
+        public List<ScheduleModel> Schedules { get; set; }
+
+        public List<RequestModel> Requests { get; set; }
+    }
+}
---
+++ WebAPI_backend/Resource-Management-System/BysinessServices/Models/ScheduleModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BysinessServices.Models
+{
+    public class ScheduleModel
+    {
+        public int Id { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public string Purpose { get; set; }
+
+        public int ResourceId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string ResourceName { get; set; }
+    }
+}
