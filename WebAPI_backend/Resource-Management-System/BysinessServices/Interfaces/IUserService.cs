132d9df882286ef17a35f77b641a93f18872c419
Ellowa <ellowa123321@gmail.com>
2023-04-22 14:04:56 +0300

    Added services Interfaces

---
+++ WebAPI_backend/Resource-Management-System/BysinessServices/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using BysinessServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BysinessServices.Interfaces
+{
+    public interface IUserService : ICrud<UserModel>
+    {
+    }
+}
