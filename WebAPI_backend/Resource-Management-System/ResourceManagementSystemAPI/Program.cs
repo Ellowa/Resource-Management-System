456447a507b6f9548baad9d5d81e032fbc749223
Roman Yeliukhin <ellowa123321@gmail.com>
2023-04-23 21:56:50 +0300

    Added Swagger to Production env

--- WebAPI_backend/Resource-Management-System/ResourceManagementSystemAPI/Program.cs
+++ WebAPI_backend/Resource-Management-System/ResourceManagementSystemAPI/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddAutoMapper(typeof(AutomapperProfile));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
