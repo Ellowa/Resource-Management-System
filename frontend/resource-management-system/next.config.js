705ff31b3de0d19bb9f3aaf797273b2e9ffd3b44
Andrey Reznikov <andreyrez02@gmail.com>
2023-04-25 22:31:34 +0300

    Add rewrites from internal to our API

--- frontend/resource-management-system/next.config.js
+++ frontend/resource-management-system/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/Resource',
+        destination: 'https://resource-ms-backend.azurewebsites.net/api/Resource',
+      },
+      {
+        source: '/api/Request',
+        destination: 'https://resource-ms-backend.azurewebsites.net/api/Request',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
