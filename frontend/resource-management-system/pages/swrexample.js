0d962b205a2a22c62036c75450da84aa6865ef49
Andrey Reznikov <andreyrez02@gmail.com>
2023-04-25 23:16:05 +0300

    Create data fetching example

---
+++ frontend/resource-management-system/pages/swrexample.js
@@ -0,0 +1,25 @@
+import GetAllResources from '../fetchers/GetAllResources';
+
+function Resources() {
+    const { resources, isLoading, isError } = GetAllResources();
+
+    if (isLoading) return <div>Loading...</div>
+    if (isError) return <div>Error</div>
+    console.log(resources)
+    return (
+        <div>
+            <h1>Resources</h1>
+            <ul>
+                {resources.map((resource) => (
+                    <li key={resource.id}>{resource.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default function Example() {
+    return (
+        <Resources />
+    )
+}
