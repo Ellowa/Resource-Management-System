a8543d64c7c7712af12347984107688b6e51182a
Andrey Reznikov <andreyrez02@gmail.com>
2023-05-02 15:27:28 +0300

    Change MainPage to use actual data

--- frontend/resource-management-system/src/components/main-page/MainPage.js
+++ frontend/resource-management-system/src/components/main-page/MainPage.js
@@ -1,4 +1,11 @@
+import { GetAllResources } from "@/fetchers/ResourceController";
+
 export function MainPage() {
+    const { resources, isLoading, isError } = GetAllResources();
+
+    if (isLoading) return <div>Loading...</div>
+    if (isError) return <div>Error</div>
+
     return (
         <div className="main-page">
             <h2 className="main-text">Доступні ресурси</h2>
@@ -17,42 +24,14 @@ export function MainPage() {
                 <div className="table__content">
                     <table cellPadding="0" cellSpacing="0" border="0">
                         <tbody>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
-                            <tr>
-                                <td>Якийсь ресурс</td>
-                                <td><button>Додати</button></td>
-                            </tr>
+                            {
+                                resources.map((resource) => (
+                                    <tr key={resource.id}>
+                                        <td>{resource.name}</td>
+                                        <td><button>Додати</button></td>
+                                    </tr>
+                                ))
+                            }
                         </tbody>
                     </table>
                 </div>
