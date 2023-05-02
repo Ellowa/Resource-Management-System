10bfd53782451bac66b8edbe2a2101796aa858cc
Daria Volobuieva <65347045+DariaVolobueva@users.noreply.github.com>
2023-05-02 20:33:23 +0300

    Add switch between pages

--- frontend/resource-management-system/src/pages/index.js
+++ frontend/resource-management-system/src/pages/index.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { ResourcePage } from "../components/resource-page/ResourcePage";
 import { SideBar } from "../components/side-bar/SideBar";
+import { RequestPage } from "../components/request-page/RequestPage";
 
 export default function Home() {
+  const [page, setPage] = useState(false);
+
   return (
     <div className="page">
-      <SideBar />
-      {/* TODO: If button on resources - Resource Page, if on requests - RequestPage, if on any other button - corresponding page */}
-      <ResourcePage />
-      {/* <RequestPage /> */}
+      <SideBar setPage = {setPage}/>
+      {page ? <RequestPage></RequestPage> : <ResourcePage></ResourcePage>}
+
     </div>
   )
 }
