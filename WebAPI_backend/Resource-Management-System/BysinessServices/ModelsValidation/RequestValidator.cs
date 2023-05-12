7cd5f7e43f077a459932e8aa1a73dac146406a4c
Roman Yeliukhin <ellowa123321@gmail.com>
2023-05-12 21:23:45 +0300

    Added RequestValidation + register validations in DI

---
+++ WebAPI_backend/Resource-Management-System/BysinessServices/ModelsValidation/RequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using BysinessServices.Models;
+using FluentValidation;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BysinessServices.ModelsValidation
+{
+    public class RequestValidator : AbstractValidator<RequestModel> 
+    {
+        public RequestValidator() 
+        { 
+            RuleFor(r => r.Start).GreaterThanOrEqualTo(DateTime.UtcNow);
+            RuleFor(r => r.End).GreaterThan(r => r.Start);
+            RuleFor(r => r.Purpose).MinimumLength(5).When(r => !r.Purpose.IsNullOrEmpty());
+            RuleFor(r => r.Id).Null();
+            RuleFor(r => r.ResourceName).Null();
+        }
+    }
+}
--- WebAPI_backend/Resource-Management-System/ResourceManagementSystemAPI/Program.cs
+++ WebAPI_backend/Resource-Management-System/ResourceManagementSystemAPI/Program.cs
@@ -8,6 +8,8 @@ using BysinessServices;
 using BysinessServices.Interfaces;
 using BysinessServices.Services;
 using ResourceManagementSystemAPI.Middleware;
+using FluentValidation.AspNetCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddFluentValidation(conf =>
+{
+    conf.RegisterValidatorsFromAssembly(typeof(Program).Assembly);
+    conf.AutomaticValidationEnabled = false;
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
     options.UseMySQL(builder.Configuration.GetConnectionString("MySQLConnection"));
