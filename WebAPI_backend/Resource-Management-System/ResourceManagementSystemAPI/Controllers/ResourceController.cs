ac5a3fcdec354d2de7b3bfd5e4c336f78b24707f
Roman Yeliukhin <ellowa123321@gmail.com>
2023-05-15 13:33:20 +0300

    Fix Updete Methods

--- WebAPI_backend/Resource-Management-System/ResourceManagementSystemAPI/Controllers/ResourceController.cs
+++ WebAPI_backend/Resource-Management-System/ResourceManagementSystemAPI/Controllers/ResourceController.cs
@@ -98,8 +98,6 @@ namespace ResourceManagementSystemAPI.Controllers
                 return ValidationProblem(validationResult.Errors.ToString());
             }
 
-            if (id != resource.Id) return BadRequest();
-
             await _resourceService.UpdateAsync(resource);
 
             return NoContent();
@@ -165,8 +163,6 @@ namespace ResourceManagementSystemAPI.Controllers
                 validationResult.AddToModelState(this.ModelState);
                 return ValidationProblem(validationResult.Errors.ToString());
             }
-
-            if (id != resourceType.Id) return BadRequest();
 
             await _resourceService.UpdateResourceTypeAsync(resourceType);
 
